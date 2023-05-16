cac036c77eee3090188bbd160b629c99b5ced3cf
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-05-16 18:39:44 +0200

    ⚙️ add script that run all the project

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Ajout de la table epytodo.sql à MySQL..."
+mysql -u root -p < server/epytodo.sql
+
+echo "Lancement du serveur Express..."
+cd server
+nodemon server.js &
+
+sleep 1
+
+echo "Lancement de l'application Flutter..."
+cd ../client
+flutter run
+
+exit 0
